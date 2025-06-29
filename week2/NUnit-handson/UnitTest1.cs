5ff73a91bdd8e625e28bf66021ad357d25ec04e0
BillyDoesDev <64155209+BillyDoesDev@users.noreply.github.com>
2025-06-29 19:47:06 +0530

    week2, q4: added remaining test cases

--- week2/NUnit-handson/UnitTest1.cs
+++ week2/NUnit-handson/UnitTest1.cs
@@ -23,4 +23,30 @@ public class CalculatorTests {
         Assert.That(result, Is.EqualTo(expected));
     }
 
+    [TestCase(5, 3, 2)]
+    [TestCase(10, 4, 6)]
+    public void Subtraction_ReturnsExpected(double a, double b, double expected) {
+        var result = _calculator!.Subtraction(a, b);
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [TestCase(2, 3, 6)]
+    [TestCase(-2, 4, -8)]
+    public void Multiplication_ReturnsExpected(double a, double b, double expected) {
+        var result = _calculator!.Multiplication(a, b);
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [TestCase(10, 2, 5)]
+    [TestCase(9, 3, 3)]
+    public void Division_ReturnsExpected(double a, double b, double expected) {
+        var result = _calculator!.Division(a, b);
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Division_ByZero_ThrowsException() {
+        Assert.Throws<System.ArgumentException>(() => _calculator!.Division(5, 0));
+    }
+
 }
