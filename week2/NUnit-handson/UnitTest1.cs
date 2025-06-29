3712d927f875240dc51794ff9dcb5f3190a50bb0
BillyDoesDev <64155209+BillyDoesDev@users.noreply.github.com>
2025-06-29 19:43:31 +0530

    week2, q4

---
+++ week2/NUnit-handson/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿namespace NUnit_handson;
+
+[TestFixture]
+public class CalculatorTests {
+    private IMathLibrary? _calculator;
+
+
+    [SetUp]
+    public void SetUp() {
+        _calculator = new SimpleCalculator();
+    }
+
+    [TearDown]
+    public void TearDown() {
+        _calculator = null;
+    }
+
+    [TestCase(2, 3, 5)]
+    [TestCase(-1, 4, 3)]
+    [TestCase(0, 0, 0)]
+    public void Addition_ReturnsExpected(double a, double b, double expected) {
+        var result = _calculator!.Addition(a, b);
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+}
